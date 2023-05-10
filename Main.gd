b6356e65b57aff9a4a1bd2063fe4c34531895e81
h3nse <henrikf.bj@gmail.com>
2023-05-10 14:04:58 +0200

    Add controls and convert to decimeters

--- Main.gd
+++ Main.gd
@@ -5,13 +5,28 @@ onready var massSlider = $MassSlider
 onready var massLabel = $MassLabel
 
 var particles = []
-var wind = Vector2(-0.5,0)
+var wind = Vector2(-10,0)
+var mouseStartPos = Vector2.ZERO
 
 const g = Vector2(0,9.8)
 
+enum {
+	NORMAL,
+	DRAWING
+}
+var state = NORMAL
+
 func _process(delta):
-	if Input.is_action_just_pressed("click"):
-		create_particle()
+	match state:
+		NORMAL:
+			if Input.is_mouse_button_pressed(BUTTON_LEFT):
+				mouseStartPos = get_local_mouse_position()
+				state = DRAWING
+		DRAWING:
+			if Input.is_action_just_released("click"):
+				create_particle(mouseStartPos, calculate_velocity(mouseStartPos, get_local_mouse_position()), massSlider.value)
+				state = NORMAL
+			update()
 
 	for p in particles:
 		p.apply_force(p.mass * g)
@@ -19,9 +34,18 @@ func _process(delta):
 	
 	massLabel.text = str(massSlider.value) + " * 10g"
 
-func create_particle():
+func calculate_velocity(startPoint : Vector2, endPoint : Vector2):
+	var velocity = startPoint.direction_to(endPoint) * startPoint.distance_to(endPoint)
+	return velocity
+
+func create_particle(_position : Vector2, _velocity : Vector2, mass : int):
 	var particle = particleShell.instance()
 	self.add_child(particle)
 	particles.append(particle)
-	particle.position = get_global_mouse_position()
-	particle.mass = massSlider.value / 100
+	particle.assign_properties(_position, _velocity, mass)
+
+func _draw():
+	if state == DRAWING:
+		draw_line(mouseStartPos, get_local_mouse_position(), Color(255,0,0), 1)
+	else:
+		pass
--- Particle.gd
+++ Particle.gd
@@ -4,10 +4,18 @@ var mass = 1
 var velocity = Vector2.ZERO
 var acceleration = Vector2.ZERO
 
-func apply_force(Force):
-	acceleration += Force / mass
+func assign_properties(_position : Vector2, _velocity : Vector2, _mass):
+	position = _position
+	velocity = _velocity
+	mass = _mass
+
+func apply_force(Force): 
+	if !mass: 
+		acceleration = Vector2.ZERO
+	else:
+		acceleration += Force / mass * 10
 
 func _process(delta):
-	velocity += acceleration * delta
-	position += velocity * delta * 100
+	velocity += acceleration * delta * 10
+	position += velocity * delta
 	acceleration = Vector2.ZERO
